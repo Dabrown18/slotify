40da34803383abba7cec50f86e0bc1cd257ad352
Darron Brown <DarronBrown88@outlook.com>
2018-09-03 11:17:38 -0600

    connected to database

---
+++ includes/config.php
@@ -0,0 +1,18 @@
+<?php
+ob_start();
+$timezone = date_default_timezone_set("America/Los_Angeles");
+
+// Database connection
+$host     = "localhost";
+$username = "root";
+$password = "root";
+$db       = "slotify";
+$socket   = 8889;
+
+$con = mysqli_connect($host, $username, $password, $db, $socket);
+
+if(mysqli_connect_errno()) {
+	echo "Failed to connect " . mysqli_connect_errno();
+}
+
+?>
