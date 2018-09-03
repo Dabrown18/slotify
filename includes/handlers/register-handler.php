2dcaa341ad6a5e189fc45e3e68684bc50c9b5f23
Darron Brown <DarronBrown88@outlook.com>
2018-09-02 20:22:56 -0600

    successful registration redirected to index

--- includes/handlers/register-handler.php
+++ includes/handlers/register-handler.php
@@ -24,8 +24,12 @@ if(isset($_POST["registerButton"])) {
 	$password  = sanitizeFormPassword($_POST["password"]);
 	$password2 = sanitizeFormPassword($_POST["password2"]);
 
-	$account -> register($username, $firstName, $lastName, $email, $email2, $password, $password2);
+	$wasSuccessful = $account -> register($username, $firstName, $lastName, $email, $email2, $password, $password2);
 
+	if($wasSuccessful) {
+		// If registration is successful user is redirected to index
+		header("Location: index.php");
+	}
 }
 
 ?>
